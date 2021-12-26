154c3cb06f46ffd78a9ffeef62dc2c47f7cabc0d
ibababooey <72231526+ibababooey@users.noreply.github.com>
2021-12-26 10:19:20 -0600

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,10 @@
+      function generate() {
+var group = document.getElementById("group").value;
+var groupn = document.getElementById("groupn").value;
+var blocked = document.getElementById("blocked").value;
+var desc = document.getElementById("desc").value;
+var ip = document.getElementById("ip").value;
+var user = document.getElementById("user").value;
+
+  window.open("https://cn1759620794-7vnsr30257.ibosscloud.com/syncCustomBranding/block_page/122385/9534/9534-block_page.html?fn="+ group +"&fp="+ groupn +"&bu="+ blocked +"%2F&bc="+ desc +"&ip="+ ip +"&ibip=cn1759617723-vnsg11042.ibosscloud.com&user="+ user +"%20&ipm=cn1759617911-vnsg11308.ibosscloud.com")
+}
